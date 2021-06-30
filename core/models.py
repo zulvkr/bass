e76fa31886013a678f79858d0a1de169d79b9f50
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-30 23:09:57 +0700

    git init

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from typing import AbstractSet
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_ambassador = models.BooleanField(default=True)
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
