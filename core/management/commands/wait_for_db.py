72178c2fd5a8518a5ee17bad6e6bf438f301b94e
zulvkr <ivan.zulf.ikar@gmail.com>
2021-07-01 08:16:18 +0700

    djangoadmin superuser

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -1,1 +1,20 @@
-from django.core.management import BaseCommand
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+import time
+
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    self.stdout.write('Waiting for the database...')
+    conn = None
+
+    while not conn:
+      try:
+        conn = connections['default']
+      except OperationalError:
+        self.stdout.write('Db unavailable, waituing for 1 second...')
+        time.sleep(1)
+
+    self.stdout.write(self.style.SUCCESS('DB available!'))
